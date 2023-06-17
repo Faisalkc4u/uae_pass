2bf633eae74ba863ea612d30a2e79aa7b69c11b4
Faisal KC <faisalkc4u@gmail.com>
2023-06-17 09:53:21 +0400

    basic uae-pass set up

---
+++ android/src/main/kotlin/com/mvpapps/uae_pass/UAEPassRequestModels.kt
@@ -0,0 +1,138 @@
+package com.mvpapps.uae_pass
+
+import ae.sdg.libraryuaepass.business.Language
+import ae.sdg.libraryuaepass.business.Environment
+import ae.sdg.libraryuaepass.business.authentication.model.UAEPassAccessTokenRequestModel
+import ae.sdg.libraryuaepass.business.documentsigning.model.DocumentSigningRequestParams
+import ae.sdg.libraryuaepass.business.documentsigning.model.UAEPassDocumentDownloadRequestModel
+import ae.sdg.libraryuaepass.business.documentsigning.model.UAEPassDocumentSigningRequestModel
+import ae.sdg.libraryuaepass.business.profile.model.UAEPassProfileRequestModel
+import ae.sdg.libraryuaepass.utils.Utils.generateRandomString
+import android.content.Context
+import android.content.pm.PackageManager
+import java.io.File
+import java.util.*
+
+ 
+object UAEPassRequestModels {
+    //UAE PASS START -- ADD BELOW FIELDS
+    private const val UAE_PASS_CLIENT_ID = "sandbox_stage"
+    private const val UAE_PASS_CLIENT_SECRET = "sandbox_stage"
+    private const val REDIRECT_URL = "https://oauthtest.com/authorization/return"
+
+    //UAE PASS END -- ADD BELOW FIELDS
+    private const val DOCUMENT_SIGNING_SCOPE = "urn:safelayer:eidas:sign:process:document"
+    private const val RESPONSE_TYPE = "code"
+    private const val SCOPE = "urn:uae:digitalid:profile"
+    private const val ACR_VALUES_MOBILE = "urn:digitalid:authentication:flow:mobileondevice"
+    private const val ACR_VALUES_WEB = "urn:safelayer:tws:policies:authentication:level:low"
+    private const val UAE_PASS_PACKAGE_ID = "ae.uaepass.mainapp"
+    private const val UAE_PASS_QA_PACKAGE_ID = "ae.uaepass.mainapp.qa"
+    private const val UAE_PASS_STG_PACKAGE_ID = "ae.uaepass.mainapp.stg"
+    private const val SCHEME = "uaepasssample"
+    private const val FAILURE_HOST = "https://oauthtest.com/authorization/return"
+    private const val SUCCESS_HOST = "https://oauthtest.com/authorization/return"
+    private val STATE = generateRandomString(24)
+     val UAE_PASS_ENVIRONMENT: Environment = Environment.STAGING
+
+    private fun isPackageInstalled(packageManager: PackageManager): Boolean {
+        val packageName = when (UAE_PASS_ENVIRONMENT) {
+            is Environment.STAGING -> {
+                UAE_PASS_STG_PACKAGE_ID
+            }
+            is Environment.QA -> {
+                UAE_PASS_QA_PACKAGE_ID
+            }
+            is Environment.PRODUCTION -> {
+                UAE_PASS_PACKAGE_ID
+            }
+            else -> {
+                UAE_PASS_PACKAGE_ID
+            }
+        }
+        var found = true
+        try {
+            packageManager.getPackageInfo(packageName, 0)
+        } catch (e: PackageManager.NameNotFoundException) {
+            found = false
+        }
+        return found
+    }
+
+    fun getAuthenticationRequestModel(context: Context): UAEPassAccessTokenRequestModel {
+        val ACR_VALUE = if (isPackageInstalled(context.packageManager)) {
+            ACR_VALUES_MOBILE
+        } else {
+            ACR_VALUES_WEB
+        }
+        return UAEPassAccessTokenRequestModel(
+            UAE_PASS_ENVIRONMENT,
+            UAE_PASS_CLIENT_ID,
+            UAE_PASS_CLIENT_SECRET,
+            SCHEME,
+            FAILURE_HOST,
+            SUCCESS_HOST,
+            REDIRECT_URL,
+            SCOPE,
+            RESPONSE_TYPE,
+            ACR_VALUE,
+            STATE,
+                Language.EN,
+
+        )
+    }
+
+    fun getDocumentRequestModel(
+        file: File?,
+        documentSigningParams: DocumentSigningRequestParams
+    ): UAEPassDocumentSigningRequestModel {
+        return UAEPassDocumentSigningRequestModel(
+            UAE_PASS_ENVIRONMENT,
+            UAE_PASS_CLIENT_ID,
+            UAE_PASS_CLIENT_SECRET,
+            SCHEME,
+            FAILURE_HOST,
+            SUCCESS_HOST,
+            Objects.requireNonNull(documentSigningParams.finishCallbackUrl),
+            DOCUMENT_SIGNING_SCOPE,
+            file!!,
+            documentSigningParams
+        )
+    }
+
+    fun getDocumentDownloadRequestModel(
+        documentName: String?,
+        documentURL: String?
+    ): UAEPassDocumentDownloadRequestModel {
+        return UAEPassDocumentDownloadRequestModel(
+            UAE_PASS_ENVIRONMENT,
+            UAE_PASS_CLIENT_ID,
+            UAE_PASS_CLIENT_SECRET,
+            DOCUMENT_SIGNING_SCOPE,
+            documentName!!,
+            documentURL!!
+        )
+    }
+
+    fun getProfileRequestModel(context: Context): UAEPassProfileRequestModel {
+        val ACR_VALUE = if (isPackageInstalled(context.packageManager)) {
+            ACR_VALUES_MOBILE
+        } else {
+            ACR_VALUES_WEB
+        }
+        return UAEPassProfileRequestModel(
+            UAE_PASS_ENVIRONMENT,
+            UAE_PASS_CLIENT_ID,
+            UAE_PASS_CLIENT_SECRET,
+            SCHEME,
+            FAILURE_HOST,
+            SUCCESS_HOST,
+            REDIRECT_URL,
+            SCOPE,
+            RESPONSE_TYPE,
+            ACR_VALUE,
+            STATE,
+                Language.EN
+        )
+    }
+}
--- android/src/main/kotlin/com/mvpapps/uae_pass/UaePassPlugin.kt
+++ android/src/main/kotlin/com/mvpapps/uae_pass/UaePassPlugin.kt
@@ -1,5 +1,6 @@
 package com.mvpapps.uae_pass
 
+import android.widget.Toast
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -7,6 +8,13 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import com.mvpapps.uae_pass.UAEPassRequestModels
+
+import ae.sdg.libraryuaepass.*
+import ae.sdg.libraryuaepass.UAEPassController.getAccessToken
+import ae.sdg.libraryuaepass.UAEPassAccessCodeCallback
+import ae.sdg.libraryuaepass.business.authentication.model.UAEPassAccessTokenRequestModel
+
 
 /** UaePassPlugin */
 class UaePassPlugin: FlutterPlugin, MethodCallHandler {
@@ -15,14 +23,37 @@ class UaePassPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
-
+  private lateinit var binding: FlutterPlugin.FlutterPluginBinding
+  private lateinit var requestModel: UAEPassAccessTokenRequestModel
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "uae_pass")
     channel.setMethodCallHandler(this)
+    binding = flutterPluginBinding;
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
+    if(call.method=="set_up_environment")
+    {
+       requestModel = UAEPassRequestModels.getAuthenticationRequestModel(binding.getApplicationContext())
+
+    }else if(call.method=="sign_in")
+    {
+      getAccessToken(binding.getApplicationContext(), requestModel, object : UAEPassAccessTokenCallback {
+        override fun getToken(accessToken: String?, state: String, error: String?) {
+          if (error != null) {
+            Toast.makeText(
+                    binding.getApplicationContext(),
+                    "Error while getting access token",
+                    Toast.LENGTH_SHORT
+            ).show()
+          } else {
+            Toast.makeText(binding.getApplicationContext(), "Access Token Received", Toast.LENGTH_SHORT)
+                    .show()
+          }
+        }
+      })
+    }
+    else if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
       result.notImplemented()
